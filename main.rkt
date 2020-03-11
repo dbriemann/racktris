0e862007b9e3a48427d2378db848a86af65fabe1
David Linus Briemann <ragingdave@mailbox.org>
2020-03-11 20:46:30 +0100

    add frame, canvas & events

--- main.rkt
+++ main.rkt
@@ -135,3 +135,73 @@
     (check-equal? (rotate-clockwise* block 4) block)
     (check-equal? (rotate-clockwise (rotate-counter-clockwise block)) block)))
 
+
+;; ............................... Frame, canvas & events
+
+(define tetris-frame%
+  (class frame%
+    (init) (super-new)
+    (define/override (on-subwindow-char receiver event)
+      (define handled? (super on-subwindow-char receiver event))
+      (if handled?
+          #t
+          (on-tetris-event event)))))
+
+(define-values (field-width field-height) (values 12 16))
+(define-values (window-width window-height)
+  (values (* field-width square-size) (* field-height square-size)))
+
+(define frame
+  (new tetris-frame% [label "Tetris"] [width window-width] [height window-height]))
+
+(define-values (current-block block-x block-y) (values L-Block 0 0))
+
+(define (on-tetris-paint canvas dc)
+  (send dc clear)
+  (define x (* block-x square-size))
+  (define y (* block-y square-size))
+  (draw-pict (block->pict current-block) dc x y))
+
+(define canvas (new canvas% [parent frame]
+                    [min-width window-width]
+                    [min-height window-height]
+                    [stretchable-width #f]
+                    [stretchable-height #f]
+                    [paint-callback on-tetris-paint]))
+
+(define (on-tetris-tick)
+  (if (< block-y field-height)
+      (set! block-y (add1 block-y))
+      (spawn-new-block))
+  (send canvas refresh))
+
+(define timer (new timer% [notify-callback on-tetris-tick] [interval 500]))
+
+(define block-count -1)
+(define (spawn-new-block)
+  (set! block-count (add1 block-count))
+  (set! current-block (list-ref
+                       all-blocks
+                       (modulo block-count (length all-blocks))))
+  (set! block-y 0)
+  (set! block-x (exact-truncate (- (/ field-width 2) 2))))
+
+(define (on-tetris-event event)
+  (case (send event get-key-code)
+    ((left) (on-left-right-move sub1))
+    ((right) (on-left-right-move add1))
+    ((up) (on-rotation rotate-clockwise))
+    ((down) (on-rotation rotate-counter-clockwise)))
+  (send canvas refresh))
+
+(define (on-rotation rotate-function)
+  (set! current-block (rotate-function current-block)))
+
+(define (on-left-right-move direction)
+  (set! block-x (direction block-x)))
+
+
+(define (start-game)
+  (spawn-new-block)
+  (send canvas focus)
+  (send frame show #t))
